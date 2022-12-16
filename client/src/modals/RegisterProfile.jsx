02af529883a807b18b92aeb6f8b5b9e0d7624dd7
Donghyun Kim <crtmt97@gmail.com>
2022-12-16 17:30:53 +0900

    feat:유저프로필 radio input 추가

--- client/src/modals/RegisterProfile.jsx
+++ client/src/modals/RegisterProfile.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import tw from 'tailwind-styled-components';
 const RegisterProfile = () => {
-  const userInputData = [
-    { 성별: ['남자', '여자'] },
-    { 나이: ['10대', '20대', '30대 이상'] },
-    { '선호 지역': ['강남', '건대', '홍대'] },
+  const userRadioInputData = [
+    { name: '성별', options: ['남자', '여자'] },
+    { name: '나이', options: ['10대', '20대', '30대 이상'] },
+    { name: '선호 지역', options: ['강남', '건대', '홍대'] },
+  ];
+  const userSelectInputData = [
+    { name: 'MBTI', options: ['남자', '여자'] },
+    { name: '선호 테마', options: ['10대', '20대', '30대 이상'] },
+    { name: '비선호 테마', options: ['강남', '건대', '홍대'] },
   ];
   return (
     <div
@@ -22,10 +27,30 @@ const RegisterProfile = () => {
       <div className='mx-auto mt-[1%] w-full text-center'>
         <input className='border rounded-full pl-[5%] w-4/5' type='text' placeholder='한 줄 소개' />
       </div>
+      {userRadioInputData.map((inputData) => (
+        <RadioInputBox inputData={inputData} key={inputData.name} />
+      ))}
+      {userSelectInputData.map((inputData) => (
+        <SelectInputBox inputData={inputData} key={inputData.name} />
+      ))}
     </div>
   );
 };
 
-const RadioInputBox = () => <div></div>;
+const RadioInputBox = ({ inputData }) => (
+  <div className='mx-[5%] w-[90%] h-10 border-b border-black flex'>
+    <div className='w-1/5'>{inputData.name}</div>
+    <div className='ml-auto w-4/5 flex'>
+      {inputData.options.map((option) => (
+        <div className='ml-2' key={inputData.name + option}>
+          <label>{option}</label>
+          <input type='radio' />
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+const SelectInputBox = () => {};
 
 export default RegisterProfile;
