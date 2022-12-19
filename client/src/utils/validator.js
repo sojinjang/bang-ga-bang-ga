9e637ead3fb418f902702899c92e97bbb10fe7af
sojinjang <jsojin6119@gmail.com>
2022-12-19 12:42:06 +0900

    :sparkles: feat(validator): 입력값 검사 함수들 추가
    이름, 번호, 전화번호, 이메일, 비밀번호 검사 함수들 추가함

---
+++ client/src/utils/validator.js
@@ -0,0 +1,24 @@
+function isName(name) {
+  return /^[가-힣]{2,4}/.test(name);
+}
+
+function isNum(numStr) {
+  return /^[0-9]+$/.test(numStr);
+}
+
+function isPhoneNumber(phoneNumberiInput) {
+  const phoneNumberRegExp = /^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$/;
+  return phoneNumberRegExp.test(phoneNumberiInput);
+}
+
+function isValidEmail(emailiInput) {
+  const emailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  return emailRegExp.test(emailiInput);
+}
+
+function isValidPassword(passwordInput) {
+  const passwordRegExp = /(?=.*\d{1,50})(?=.*[~`!@#$%\^&*()-+=]{1,50})(?=.*[a-zA-Z]{2,50}).{8,50}$/;
+  return passwordRegExp.test(passwordInput);
+}
+
+export { isName, isNum, isPhoneNumber, isValidEmail, isValidPassword };
