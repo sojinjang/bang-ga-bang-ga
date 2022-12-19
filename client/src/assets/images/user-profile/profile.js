19f5f21057f5cf55ffb1f3b86e69e4cade82f37f
wooooooongs <pires11233@gmail.com>
2022-12-19 16:46:36 +0900

    :sparkles: feat: 임시 프로필 이미지들 하나의 JS에서 가져오는 코드 추가

---
+++ client/src/assets/images/user-profile/profile.js
@@ -0,0 +1,9 @@
+import 동현 from './동현.jpg';
+import 선아 from './선아.png';
+import 소진 from './소진.jpg';
+import 동하 from './동하.png';
+import 승빈 from './승빈.png';
+import 재웅 from './재웅.png';
+import 지현 from './지현.jpeg';
+
+export { 동현, 선아, 소진, 동하, 승빈, 재웅, 지현 };
