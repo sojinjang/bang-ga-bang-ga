a278c18db4ab0f8a1019cd6145a7fcf4737cfed3
wooooooongs <pires11233@gmail.com>
2022-12-20 00:04:02 +0900

    :sparkles: feat: 모집 페이지 라우트 추가

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,13 +5,14 @@ import Login from '../pages/Login';
 import MyPage from '../pages/MyPage';
 import NotFound from '../pages/NotFound';
 import Register from '../pages/Register';
+import RecruitList from '../pages/recruit-list/RecruitList';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/recruit-list' element={<Home />} />
+        <Route path='/recruit-list' element={<RecruitList />} />
         <Route path='/recruit-map' element={<Home />} />
         <Route path='/cafeinfo' element={<Home />} />
         <Route path='/mypage' element={<MyPage />}>
