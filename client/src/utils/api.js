bff4a6c0c7d8928bfa8884488c8730e4814027fb
sojinjang <jsojin6119@gmail.com>
2022-12-19 22:01:03 +0900

    :sparkles: feat(api): api 요청 함수들 추가
    1. get, post, postImg, patch, delete 요청
    2. postImg를 제외한 요청은  header에 토큰값 넣음
    3. response로 에러 받아오면 애러 코드를 키값으로 상수 파일에서 error message 받아옴

---
+++ client/src/utils/api.js
@@ -0,0 +1,104 @@
+import { getCookieValue } from './cookie.js';
+import { ErrorMessage } from '../constants/ErrorMessage.js';
+import { Keys } from '../constants/Keys.js';
+
+async function get(endpoint, params = '') {
+  const apiUrl = params === '' ? endpoint : `${endpoint}/${params}`;
+  const res = await fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${getCookieValue(Keys.LOGIN_TOKEN)}`,
+    },
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(ErrorMessage[error.errorCode]);
+  }
+
+  const result = await res.json();
+  return result;
+}
+
+async function post(endpoint, data) {
+  const apiUrl = endpoint;
+  const bodyData = JSON.stringify(data);
+  const res = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${getCookieValue(Keys.LOGIN_TOKEN)}`,
+    },
+    body: bodyData,
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(ErrorMessage[error.errorCode]);
+  }
+
+  const result = await res.json();
+  return result;
+}
+
+async function postImg(endpoint, data) {
+  const apiUrl = endpoint;
+  const res = await fetch(apiUrl, {
+    method: 'POST',
+    body: data,
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(ErrorMessage[error.errorCode]);
+  }
+
+  const result = await res.json();
+  return result;
+}
+
+async function patch(endpoint, params = '', data) {
+  const apiUrl = params === '' ? endpoint : `${endpoint}/${params}`;
+  const bodyData = JSON.stringify(data);
+  const res = await fetch(apiUrl, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${getCookieValue(Keys.LOGIN_TOKEN)}`,
+    },
+    body: bodyData,
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(ErrorMessage[error.errorCode]);
+  }
+
+  const result = await res.json();
+  return result;
+}
+
+async function del(endpoint, params = '', data = {}) {
+  const apiUrl = params === '' ? endpoint : `${endpoint}/${params}`;
+  const bodyData = JSON.stringify(data);
+
+  const res = await fetch(apiUrl, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${getCookieValue(Keys.LOGIN_TOKEN)}`,
+    },
+    body: bodyData,
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(ErrorMessage[error.errorCode]);
+  }
+
+  const result = await res.json();
+  return result;
+}
+
+export { get, post, postImg, patch, del as delete };
