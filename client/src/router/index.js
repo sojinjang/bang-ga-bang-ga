217df7db115af7af9ee569a8071e13f754e53178
Donghyun Kim <crtmt97@gmail.com>
2022-12-20 05:08:50 +0000

    ✨feat:회원정보수정 페이지 및 기타

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../pages/Login';
 import MyPage from '../pages/MyPage';
 import NotFound from '../pages/NotFound';
 import Register from '../pages/Register';
+import EditUserInfo from '../pages/Mypage/EditUserInfo';
 
 const Router = () => {
   return (
@@ -14,14 +15,10 @@ const Router = () => {
         <Route path='/recruit-list' element={<Home />} />
         <Route path='/recruit-map' element={<Home />} />
         <Route path='/cafeinfo' element={<Home />} />
-        <Route path='/mypage' element={<MyPage />}>
-          <Route path='edit' element={<Home />} />
-          <Route path='confirm-pwd' element={<Home />} />
-          <Route path='matching-history' element={<Home />} />
-        </Route>
+        <Route path='/mypage' element={<MyPage />} />
+        <Route path='/mypage/edit' element={<EditUserInfo />} />
         <Route path='/register' element={<Register />}></Route>
         <Route path='/login' element={<Login />}></Route>
-
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
