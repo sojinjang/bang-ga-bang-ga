bf63bc8a04a88d557769252c16945c8a1177dd65
wooooooongs <pires11233@gmail.com>
2022-12-20 21:34:26 +0900

    :lipstick: style: 글쓰기 두 번째 모달창 마크업

--- client/src/pages/recruit-list/RecruitList.jsx
+++ client/src/pages/recruit-list/RecruitList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import tw from 'tailwind-styled-components';
 import { RegionButton } from '../../components/buttons/Buttons';
 import RecruitTypeIcon from '../../components/recruit/RecruitTypeIcon';
@@ -58,14 +58,22 @@ const RecruitList = () => {
 const Modal = () => {
   const [showRecruitPost, setShowRecruitPost] = useRecoilState(showRecruitPostAtom);
   const [showRecruitModalPage, setShowRecruitModalPage] = useRecoilState(showRecruitModalPageAtom);
+  const [firstModalData, setFirstModalData] = useState({
+    title: '',
+    count: 2,
+    date: '',
+  });
 
   const FirstModal = () => {
     return (
-      <div>
-        <div className='flex justify-between mt-10 mx-[110px]'>
-          <div className='flex flex-col'>
+      <div className=' mt-10 ml-[40px]'>
+        <div className='flex'>
+          <div className='flex flex-col mr-[20px]'>
             <span>제목</span>
-            <input className='w-[300px] h-[50px] p-3 border border-solid border-gray-400' />
+            <input
+              placeholder='제목을 입력하세요'
+              className='w-[300px] h-[45px] p-3 border border-solid border-gray-400'
+            />
           </div>
           <div className='flex flex-col'>
             <span>인원</span>
@@ -74,14 +82,17 @@ const Modal = () => {
               placeholder='2'
               min={2}
               max={8}
-              className='w-[60px] h-[50px] p-3 border border-solid border-gray-400'
+              className='w-[60px] h-[45px] p-3 border border-solid border-gray-400'
+              onKeyDown={(event) => {
+                event.preventDefault();
+              }}
             />
           </div>
         </div>
         <div className='flex mt-5'>
-          <div className='flex flex-col mx-[110px]'>
+          <div className='flex flex-col'>
             <span>접선 시간</span>
-            <input type='date' className='w-[300px] h-[50px] p-3 border border-solid border-gray-400' />
+            <input type='datetime-local' className='w-[300px] h-[45px] p-3 border border-solid border-gray-400' />
           </div>
         </div>
         <div>
@@ -92,7 +103,9 @@ const Modal = () => {
           </button>
           <button
             className='w-[60px] h-[35px] right-8 bottom-6 bg-sky-500/50 drop-shadow-lg rounded-lg align-middle absolute'
-            onClick={() => setShowRecruitModalPage(2)}>
+            onClick={() => {
+              setShowRecruitModalPage(2);
+            }}>
             다음
           </button>
         </div>
@@ -100,9 +113,100 @@ const Modal = () => {
     );
   };
 
+  const SecondModal = () => {
+    return (
+      <div className='flex'>
+        <div className='mt-10 ml-[40px] mr-[30px]'>
+          <p>지역</p>
+          <ul className='flex justify-between'>
+            <li>
+              <input type='radio' value={'홍대'} name='region' className='mr-2' />
+              <span>홍대</span>
+            </li>
+            <li>
+              <input type='radio' value={'강남'} name='region' className='mr-2' />
+              <span>강남</span>
+            </li>
+            <li>
+              <input type='radio' value={'건대'} name='region' className='mr-2' />
+              <span>건대</span>
+            </li>
+          </ul>
+          <div className='flex flex-col mt-5'>
+            <span>방문 카페명</span>
+            <input
+              type='text'
+              placeholder='방문 예정인 카페명을 입력하세요.'
+              className='w-[300px] h-[45px] p-3 border border-solid border-gray-400'
+            />
+          </div>
+          <div className='flex flex-col mt-4'>
+            <span>방문 테마명</span>
+            <input
+              type='text'
+              placeholder='방문 예정인 카페의 테마명을 입력하세요.'
+              className='w-[300px] h-[45px] p-3 border border-solid border-gray-400'
+            />
+          </div>
+          <div>
+            <button
+              className='w-[60px] h-[35px] right-[168px] bottom-6 bg-gray-400 drop-shadow-lg rounded-lg align-middle absolute '
+              onClick={() => setShowRecruitPost(false)}>
+              닫기
+            </button>
+            <button
+              className='w-[60px] h-[35px] right-[100px] bottom-6 bg-gray-400/50 drop-shadow-lg rounded-lg align-middle absolute'
+              onClick={() => {
+                setShowRecruitModalPage(1);
+              }}>
+              이전
+            </button>
+            <button
+              className='w-[60px] h-[35px] right-8 bottom-6 bg-sky-500/50 drop-shadow-lg rounded-lg align-middle absolute'
+              onClick={() => {}}>
+              제출
+            </button>
+          </div>
+        </div>
+        <div className='mt-[126px]'>
+          <div className='flex flex-col text-sm'>
+            <span>장르</span>
+            <span>난이도</span>
+            <span>활동성</span>
+            <span>추천인원</span>
+            <span>플레이타임</span>
+          </div>
+        </div>
+        <div className='ml-3 mt-[126px]'>
+          <div className='flex flex-col text-sm pl-3 border-l border-solid border-gray-500/20'>
+            <span className='text-blue-500 font-bold'>추리 / 스릴러</span>
+            <span className='text-blue-500 font-bold'>어려움</span>
+            <ul className='flex gap-2'>
+              <li>적음</li>
+              <li>보통</li>
+              <li className='text-blue-500 font-bold'>많음</li>
+            </ul>
+            <ul className='flex gap-1'>
+              <li>2인</li>
+              <li>3인</li>
+              <li className='text-blue-500 font-bold'>4인</li>
+              <li>5인 이상</li>
+            </ul>
+            <span className='text-blue-500 font-bold'>50분</span>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div className='rounded-xl absolute top-[70px] w-[600px] h-[280px] bg-slate-100 drop-shadow-lg'>
-      <FirstModal />
+    <div className='rounded-xl absolute top-[70px] w-[600px] h-[350px] bg-slate-100 drop-shadow-lg'>
+      <div className={showRecruitModalPage === 1 ? '' : 'hidden'}>
+        <FirstModal />
+      </div>
+      <div className={showRecruitModalPage === 2 ? '' : 'hidden'}>
+        <SecondModal />
+      </div>
     </div>
   );
 };
