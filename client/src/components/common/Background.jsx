09c213104b86a8ceeddcd62b6c20bf5105e15288
Donghyun Kim <crtmt97@gmail.com>
2022-12-21 18:02:46 +0000

    Feat home

--- client/src/components/common/Background.jsx
+++ client/src/components/common/Background.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Background = ({ img, children }) => {
   return (
     <div
-      className='w-full h-screen flex justify-center items-center flex-col bg-cover'
+      className='w-full h-screen flex items-center flex-col bg-cover'
       style={{ backgroundImage: `url(/images/backgrounds/${img}.png)` }}>
       {children}
     </div>
