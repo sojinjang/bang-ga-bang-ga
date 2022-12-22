7b36704083cd612f5f56de4f989c4151bb667030
wooooooongs <pires11233@gmail.com>
2022-12-23 01:44:16 +0900

    :sparkles: feat(recruitList): API통신 후 페이지네이션 구현 완료

--- client/src/pages/recruit-list/RecruitList.jsx
+++ client/src/pages/recruit-list/RecruitList.jsx
@@ -17,6 +17,19 @@ const RecruitList = () => {
   const setScreenLevel = useSetRecoilState(screenLevelAtom);
   const REGION_DATA = ['홍대', '강남', '건대'];
 
+  const [postData, setPostData] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const fetchRecruitData = async (pageNumber) => {
+    const data = await get('http://localhost:3008/api/matching-posts', pageNumber);
+
+    setPostData(data);
+  };
+
+  useEffect(() => {
+    fetchRecruitData(pageNumber);
+  }, [pageNumber]);
+
   const handleResize = () => {
     window.innerHeight < 985 ? setScreenLevel(2) : setScreenLevel(1);
   };
@@ -74,34 +87,44 @@ const RecruitList = () => {
             <ListItem />
             <ListItem />
             {showRecruitPost && <Modal />}
-            <svg
-              className='absolute top-[320px] -right-[80px]'
-              width='80'
-              height='80'
-              viewBox='0 0 8 14'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'>
-              <path
-                d='M5.17168 6.99999L0.22168 2.04999L1.63568 0.635986L7.99968 6.99999L1.63568 13.364L0.22168 11.95L5.17168 6.99999Z'
-                fill='black'
-              />
-            </svg>
-            <svg
-              className='absolute top-[320px] -left-[80px]'
-              width='80'
-              height='80'
-              viewBox='0 0 8 14'
-              fill='none'
-              xmlns='http://www.w3.org/2000/svg'>
-              <path
-                d='M2.828 6.99999L7.778 11.95L6.364 13.364L0 6.99999L6.364 0.635986L7.778 2.04999L2.828 6.99999Z'
-                fill='black'
-              />
-            </svg>
           </ListItemContainer>
           <PaginationButton>
-            <button className='w-[25px] h-[25px] text-sm mx-1 text-white bg-blue-1 border-solid border-[0.5px] rounded border-white'>
-              1
+            <button
+              className='w-[30px] h-[30px] text-sm mx-1 text-white bg-gray-400 border-solid border-[0.5px] rounded border-white'
+              onClick={() => {
+                pageNumber > 1 ? setPageNumber((prevNumber) => prevNumber - 1) : false;
+              }}>
+              <svg
+                className='mx-auto'
+                width='7'
+                height='14'
+                viewBox='0 0 8 14'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  d='M2.828 6.99999L7.778 11.95L6.364 13.364L0 6.99999L6.364 0.635986L7.778 2.04999L2.828 6.99999Z'
+                  fill='white'
+                />
+              </svg>
+            </button>
+            <button className='w-[30px] h-[30px] text-sm mx-1 text-white bg-blue-1 border-solid border-[0.5px] rounded border-white'>
+              {pageNumber}
+            </button>
+            <button
+              className=' w-[30px] h-[30px] text-sm mx-1 text-white bg-gray-400 border-solid border-[0.5px] rounded border-white'
+              onClick={() => setPageNumber((prevNumber) => prevNumber + 1)}>
+              <svg
+                className='mx-auto'
+                width='7'
+                height='14'
+                viewBox='0 0 8 14'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  d='M5.17168 6.99999L0.22168 2.04999L1.63568 0.635986L7.99968 6.99999L1.63568 13.364L0.22168 11.95L5.17168 6.99999Z'
+                  fill='white'
+                />
+              </svg>
             </button>
           </PaginationButton>
         </MainContainer>
