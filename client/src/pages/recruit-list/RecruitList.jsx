32d5c7256bc21ceb2e080ace5a72081b68dfcb4a
wooooooongs <pires11233@gmail.com>
2022-12-24 03:23:40 +0900

    :sparkles: feat(recruitList): 모집글 전체 데이터 받아오기 및 페이지네이션 테스트

--- client/src/pages/recruit-list/RecruitList.jsx
+++ client/src/pages/recruit-list/RecruitList.jsx
@@ -1,15 +1,18 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import tw from 'tailwind-styled-components';
 import {
   showRecruitPostAtom,
-  showRecruitModalPageAtom,
   screenLevelAtom,
   showUserProfileModalAtom,
+  currentPageAtom,
 } from '../../recoil/recruit-list/index';
 import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil';
 import { get } from '../../utils/api';
-import RecuitPostContainer from '../../components/recruit/RecruitPostContainer';
 
+import RecuitPostContainer from '../../components/recruit/RecruitPostContainer';
+import PostModal from '../../components/recruit/PostModal';
+import PaginationButton from '../../components/recruit/PageinationButton';
+import UserProfileModal from '../../components/recruit/UserProfileModal';
 import { RegionButton } from '../../components/buttons/Buttons';
 import Navigators from '../../components/common/Navigators';
 import Background from '../../components/common/Background';
@@ -18,22 +21,39 @@ const RecruitList = () => {
   document.title = '방가방가 모집글 리스트';
 
   const [showRecruitPost, setShowRecruitPost] = useRecoilState(showRecruitPostAtom);
-  const setScreenLevel = useSetRecoilState(screenLevelAtom);
   const showUserProfileModal = useRecoilValue(showUserProfileModalAtom);
-  const REGION_DATA = ['홍대', '강남', '건대'];
+  const currentPage = useRecoilValue(currentPageAtom);
+  const REGION_DATA = ['전체', '홍대', '강남', '건대'];
 
-  const [postData, setPostData] = useState([]);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const fetchRecruitData = async (pageNumber) => {
-    const data = await get('http://localhost:3008/api/matching-posts', pageNumber);
-
-    setPostData(data);
-  };
+  const [fetchedData, setFetchedData] = useState([]);
+  const [currentRegion, setCurrentRegion] = useState('');
+  const [checkRecruitingPost, setCheckRecruitingPost] = useState(false);
+  const [slicedData, setSlicedData] = useState([]);
+  const [currentPageData, setCurrentPageData] = useState([]);
 
   useEffect(() => {
-    fetchRecruitData(pageNumber);
-  }, [pageNumber]);
+    const fetchRecruitData = async () => {
+      const data = await get('http://localhost:3008/api/matching-posts');
+
+      setFetchedData(data);
+    };
+
+    fetchRecruitData();
+  }, []);
+
+  useEffect(() => {
+    let dataArray = [];
+
+    for (let i = 0; i < fetchedData.length; i += 6) {
+      dataArray.push(fetchedData.slice(i, i + 6));
+    }
+
+    setSlicedData(dataArray);
+  }, [fetchedData]);
+
+  useEffect(() => {
+    setCurrentPageData(slicedData[currentPage]);
+  }, [slicedData]);
 
   return (
     <Background img={'bg1'}>
@@ -44,7 +64,12 @@ const RecruitList = () => {
             <ul className='flex flex-row justify-center mx-auto'>
               {REGION_DATA.map((data, index) => (
                 <li key={index}>
-                  <RegionButton title={data} />
+                  <RegionButton
+                    title={data}
+                    onClick={() => {
+                      setCurrentRegion(data);
+                    }}
+                  />
                 </li>
               ))}
             </ul>
@@ -52,7 +77,13 @@ const RecruitList = () => {
           <div className='flex justify-end drop-shadow-xl gap-[50px] mb-3 mr-[10vw]'>
             <FilterContainer>
               <label className='flex mb-1 text-gray-100 justify-around'>
-                <input className='required:border-red-500' type='checkbox' />
+                <input
+                  className='required:border-red-500'
+                  onClick={() => {
+                    setCheckRecruitingPost(!checkRecruitingPost);
+                  }}
+                  type='checkbox'
+                />
                 모집중만 보기
               </label>
               <div>
@@ -72,13 +103,18 @@ const RecruitList = () => {
             </button>
           </div>
           <ListItemContainer>
-            {postData.map((post, index) => (
+            {/* 
+              memo 재웅: 오류 발생(이슈 참조)으로 현재 주석처리한 코드입니다.
+              currentPageData: fetchedData를 활용하여 페이지네이션을 구현한 변수
+            */}
+            {/* {currentPageData.map((post, index) => (
+              <RecuitPostContainer postData={post} key={index} />
+            ))} */}
+            {fetchedData.map((post, index) => (
               <RecuitPostContainer postData={post} key={index} />
             ))}
-            {showUserProfileModal && (
-              <div className='w-[450px] h-[600px] absolute bg-white top-[50%] left-[50%] translate-x-[-50%] translate-y-[-70%]'></div>
-            )}
-            {showRecruitPost && <Modal />}
+            {showUserProfileModal && <UserProfileModal />}
+            {showRecruitPost && <PostModal />}
           </ListItemContainer>
 
           <PaginationButton />
@@ -95,16 +131,11 @@ mt-10
 relative
 `;
 
-const PaginationButton = tw.div`
-  flex justify-center mt-4
-  flex justify-center mt-4
-`;
-
 const FilterContainer = tw.div`
   flex flex-col
   flex flex-col
 `;
 
 const ListItemContainer = tw.div`
-  grid grid-cols-3 grid-rows-2 gap-y-10 w-[1000px] mx-auto justify-items-center relative
+  grid grid-cols-3 grid-rows-2 gap-y-6 w-[1000px] mx-auto justify-items-center relative
 `;
