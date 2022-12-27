7d1791edea0d16714667777d09a09319bc2abb23
wooooooongs <pires11233@gmail.com>
2022-12-28 03:07:10 +0900

    :sparkles: feat(recruitList): 유저 프로필 클릭 시 postId, userIndex 저장
    리코일을 활용한 전역 상태에 저장

--- client/src/components/recruit/UserProfileContainer.jsx
+++ client/src/components/recruit/UserProfileContainer.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useRecoilState } from 'recoil';
-import { showUserProfileModalAtom } from '../../recoil/recruit-list/index';
+import React, { useEffect, useState } from 'react';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { showUserProfileModalAtom, currentPostIdAtom, currentUserIndexAtom } from '../../recoil/recruit-list/index';
 import { get } from '../../utils/api';
+import { ApiUrl } from '../../constants/ApiUrl';
 
 import crown from '../../assets/images/icon/crown.png';
 
 const UserProfileContainer = ({ postId }) => {
   const [showUserProfileModal, setShowUserProfileModal] = useRecoilState(showUserProfileModalAtom);
+  const setCurrentPostId = useSetRecoilState(currentPostIdAtom);
+  const setCurrentUserIndex = useSetRecoilState(currentUserIndexAtom);
+  const [currentTeamInfo, setCurrentTeamInfo] = useState([]);
 
   const userArray = async () => {
-    const data = await get('/api/matching-situation/post', postId);
-    console.log(data);
-
-    return data;
+    const data = await get(ApiUrl.MATCHING_POST_INFO, postId);
+    setCurrentPostId(postId);
+    setCurrentTeamInfo(data);
   };
 
   useEffect(() => {
@@ -23,6 +25,22 @@ const UserProfileContainer = ({ postId }) => {
   return (
     <div>
       <img className='absolute w-[30px] top-2 left-[40px]' src={crown} alt='' />
+      <div className='mt-8 grid gap-3 grid-cols-3 grid-rows-3'>
+        {currentTeamInfo.length > 0 &&
+          currentTeamInfo.map((user, index) => (
+            <div className='flex flex-col text-center' key={index}>
+              <img
+                onClick={() => {
+                  setCurrentUserIndex(index);
+                  setShowUserProfileModal(!showUserProfileModal);
+                }}
+                className='mx-auto w-[50px] h-[50px] drop-shadow-xl object-cover rounded-full border-solid border-[0.5px] border-gray-500 cursor-pointer'
+                src={user['profileImg']}
+                alt='유저 프로필'
+              />
+              <span className='text-black text-xs'>{user['userName']}</span>
+            </div>
+          ))}
       </div>
     </div>
   );
