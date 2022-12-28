3c2a393e19f3f94909ba100786324e38924b8dae
Suna Oh <sunna55125@gmail.com>
2022-12-28 06:39:56 +0000

    ✨feat: 카페리스트 썸네일 적용 및 정렬옵션 수정 (!42)

--- client/src/pages/CafeList.jsx
+++ client/src/pages/CafeList.jsx
@@ -64,14 +64,23 @@ const CafeList = () => {
     newList.sort((a, b) => b.reviewsSum - a.reviewsSum);
     setList(newList);
   };
+  const sortByName = () => {
+    const newList = [...list];
+    newList.sort((a, b) => {
+      return a.cafeName < b.cafeName ? -1 : a.cafeName > b.cafeName ? 1 : 0;
+    });
+
+    setList(newList);
+  };
   const optionsArray = [
+    { optionName: '카페 이름순', cbFunc: () => sortByName() },
     { optionName: '평점순', cbFunc: () => sortByStarRate() },
     { optionName: '리뷰 많은 순', cbFunc: () => sortByReviewsSum() },
   ];
   return (
     <Background img={'bg2'}>
       <Navigators />
-      <div className='flex flex-row justify-center mx-auto my-5'>
+      <div className='flex flex-row justify-center mx-auto my-5 font '>
         {detailRegion.map((region, index) => (
           <button
             className='purpleButton mx-1 font-custum_heading'
@@ -88,26 +97,30 @@ const CafeList = () => {
         ))}
       </div>
 
-      <div className='border-4 border-blue-500 w-[1200px] h-[600px] flex flex-col '>
+      <div className='w-[1200px] h-[600px] flex flex-col '>
         <div className='w-[1200px] h-[50px] flex items-start justify-end'>
           <SelectOption
             selectedOption={selected}
             setSelectedOption={setSelected}
+            pageReset={() => setPage(1)}
             cbFuncObjs={optionsArray}
             width={'w-40'}
           />
         </div>
 
-        <div className='border border-red-600 w-[1200px] h-[500px] grid grid-cols-3 grid-rows-3 gap-x-4 gap-y-6'>
-          {pagePerList.map(({ cafeId, cafeName, address, homePage, starRate, reviewsSum }, i) => {
+        <div className='w-[1200px] h-[500px] grid grid-cols-3 grid-rows-3 gap-x-4 gap-y-6 '>
+          {pagePerList.map(({ cafeId, cafeName, address, homePage, starRate, reviewsSum, cafeImg }, i) => {
+            console.log(cafeId, cafeName, address, homePage, starRate, reviewsSum, cafeImg);
             return (
-              <div className='border flex px-[27px] items-center' key={cafeId + i}>
-                <div className='border w-[100px] h-[100px]'>
-                  <img></img>
+              <div
+                className='rounded-lg bg-gray-300 shadow-md flex px-[27px] items-center hover:bg-gray-400'
+                key={`${cafeId}` + `${i}`}>
+                <div className='flex justify-center items-center min-w-[100px] min-h-[100px] relative overflow-hidden'>
+                  <img className='rounded-lg absolute w-full ' src={process.env.PUBLIC_URL + `${cafeImg}`}></img>
                 </div>
                 <div className='ml-3'>
                   <p className='flex w-full justify-start align-center'>
-                    <div>{cafeName}</div>
+                    <p className='font-bold'>{cafeName}</p>
                     <a
                       href={`https://search.naver.com/search.naver?&query=${cafeName}`}
                       target='_blank'
@@ -118,8 +131,10 @@ const CafeList = () => {
                       />
                     </a>
                   </p>
-                  <p>{address}</p>
-                  <p>{homePage}</p>
+                  <p className='text-sm'>{address}</p>
+                  <a className='text-sm' href={`${homePage}`} target='_blank' rel='noopener noreferrer nofollow'>
+                    {homePage}
+                  </a>
                   <p>
                     평점 {starRate}/10 | 리뷰 {reviewsSum}개
                   </p>
