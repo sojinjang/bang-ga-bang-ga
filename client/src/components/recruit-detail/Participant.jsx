4e9f3f4d5f4794b62f3c6ad1efac6256e1c438a8
ULemong <maintain9912@gmail.com>
2022-12-28 14:36:24 +0900

    :sparkles: feat: 참가자 정보 api 연결, 참가자 강퇴 기능 구현

--- client/src/components/recruit-detail/Participant.jsx
+++ client/src/components/recruit-detail/Participant.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import detective from '../../assets/images/icon/detective.png';
+import closeBtn from '../../assets/images/icon/close.png';
+import { post } from '../../utils/api';
 import tw from 'tailwind-styled-components';
 
-const Participant = ({ participantList }) => {
+const Participant = ({ isLeader, participantList, postId, memberListData }) => {
+  const deleteData = async (userId) => {
+    await post('/api/matching-situation/leader', { matchingPostsId: postId, userId });
+    memberListData();
+  };
+
+  const handleKickOut = (participant) => {
+    const userId = participant.userId;
+    // 참가 취소
+    deleteData(userId);
+  };
+
   return (
     <>
       {participantList.map((participant) => (
-        <Container key={participant.nick_name}>
+        <Container key={participant.nickName}>
           <Count>
             <img src={detective} alt='탐정 이모지' className='w-[20px] h-[23px] inline-block pt-[3px]' />
-            <span className='pl-[3px]'>{participant.count}</span>
+            <span className='pl-[3px] font-semibold mb-[10px]'>{participant.count}</span>
           </Count>
-          <ProfileImg src={participant.profile_img} alt='프로필 이미지' />
-          <NickName>{participant.nick_name}</NickName>
-          <div>
-            <Score>{participant.escape}</Score>
-            <Score>{participant.manner}</Score>
+          {isLeader && (
+            <button onClick={() => handleKickOut(participant)}>
+              <img className='w-5 h-5' src={closeBtn} alt='삭제 버튼' />
+            </button>
+          )}
+          <ProfileImg src={detective} alt='프로필 이미지' />
+          <NickName>{participant.nickName}</NickName>
+          <div className='flex justify-between mx-[40px]'>
+            <Score>{participant.escapeScore}</Score>
+            <Score>{participant.mannerScore}</Score>
           </div>
         </Container>
       ))}
@@ -26,29 +44,31 @@ const Participant = ({ participantList }) => {
 export default Participant;
 
 const Container = tw.div`
+  border-slate-100
+  border-[3px]
   w-[250px]
   h-[350px]
   inline-block
   mx-[20px]
-  my-[50px]
+  mt-[140px]
+  mb-[80px]
   rounded-[15px]
-  bg-[#FF5ED2]
   bg-gradient-to-b
-  from-[#ff9900]
-  to-[#ffc047]
+  from-[#3e7dab]
+  to-[#c994c2]
+
   text-center
 `;
 
 const ProfileImg = tw.img`
-  w-[160px]
-  h-[160px]
+  w-[180px]
+  h-[180px]
   rounded-[50%]
   border-4
-  border-cyan-500
+  border-[#e9b306]
   object-cover
   shadow-lg
   mx-auto
-
 `;
 
 const Count = tw.span`
@@ -58,10 +78,11 @@ const Count = tw.span`
   mt-[5px]
 `;
 
-const NickName = tw.span`
+const NickName = tw.div`
   text-3xl
   font-bold
   text-white
+  my-[18px]
 `;
 
 const Score = tw.span`
