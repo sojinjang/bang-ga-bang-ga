ca7fb5d219d0d669832f80bbc7a19c99571e982c
Donghyun Kim <crtmt97@gmail.com>
2023-01-09 17:16:41 +0900

    fix:드롭다운 필요없는 메뉴 삭제

--- client/src/constants/dropdownMenus.js
+++ client/src/constants/dropdownMenus.js
@@ -2,6 +2,5 @@ export const MENUS = [
   { name: '마이페이지', link: '/mypage' },
   { name: '회원정보 수정', link: '/mypage/edit' },
   { name: '매칭 이력', link: '/matching-list' },
-  { name: '모집글 관리', link: '/mypage' },
   { name: '로그아웃', link: '/' },
 ];
