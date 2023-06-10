4882256ccb581a845e4a5d4cd51ebc9e24327afa
sojinjang <jsojin6119@gmail.com>
2023-06-11 03:05:23 +0900

    :sparkles: feat: 기본 인포윈도우 스타일 숨기는 기능 추가

--- client/src/components/recruit-map/InfoWindow.jsx
+++ client/src/components/recruit-map/InfoWindow.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export const InfoWindow = ({ cafeName, recruitingNum }) => {
+  const customOverlayRef = useRef(null);
+
+  useEffect(() => {
+    if (customOverlayRef.current) {
+      const customOverlay = customOverlayRef.current;
+      customOverlay.parentElement.parentElement.style.border = 'none';
+      customOverlay.parentElement.parentElement.style.backgroundColor = 'transparent';
+      customOverlay.parentElement.parentElement.style.width = '180px';
+      customOverlay.parentElement.previousSibling.style.background = 'none';
+    }
+  }, []);
+
   return (
-    <div id='controlCustomOverlay' className='bg-white'>
+    <div ref={customOverlayRef} className='wrap rounded-2xl bg-purple-200 bg-opacity-80 p-2.5'>
       <div className='title text-sm font-medium'>{cafeName}</div>
-      <div className='recruiting-info text-xs'>방가방가에서 {recruitingNum}팀 모집중</div>
+      <div className='recruiting-info text-xs'>방가방가에서 모집중인 팀 {recruitingNum}팀</div>
     </div>
   );
 };
